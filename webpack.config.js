a27d88d205ed83a1e7d14309414894a2e335b6a1
jefbar1974 <jefbar@gmail.com>
2022-11-07 19:24:51 -0300

    Aula15 - ajustes

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,12 @@ module.exports = {
         static: path.join(__dirname, './'), // Abrindo o arquivo index.html da raiz
         port: 3000 // Disponibilizando a porta 300 da nossa máquina para o servidor NodeJS
     },
-    entry: path.resolve(__dirname, './', 'main.js'), // Arquivo que o webpack vai iniciar a ler
+    entry: {
+        main: path.resolve(__dirname, './', 'main.js'), // Arquivo que o webpack vai iniciar a ler
+        gerente: path.resolve(__dirname, './', 'gerente.js'), // Arquivo que o webpack vai iniciar a ler
+    },
     output: { // Pasta e arquivo que será enviada a compilação do seu projeto em uma build
-        filename: '[name]-[hash].js', 
+        filename: '[name]-[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     // Modo - development ou production
@@ -46,7 +49,16 @@ module.exports = {
     plugins: [
         // Plugin para injetar o bundle no arquivo HTML
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, "./", 'index.html')
+            // template: path.resolve(__dirname, "./", 'index.html')
+            template: 'index.html',
+            filename: 'index.html',
+            chunks: ['main']
+        }),
+        new HTMLWebpackPlugin({
+            // template: path.resolve(__dirname, "./", 'index.html')
+            template: 'gerente.html',
+            filename: 'gerente.html',
+            chunks: ['gerente']
         }),
     ]
 }
