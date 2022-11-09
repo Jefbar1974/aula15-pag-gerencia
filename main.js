f7dd46c00ef00e70aa96c4f57ebe9443ad74fc13
jefbar1974 <jefbar@gmail.com>
2022-11-08 21:55:28 -0300

    Projeto aula15

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import "./style.scss";
+
+
+executar()
+
+async function executar() {
+
+    const valoresEncontrados = await buscarValoresNaAPI()
+
+    let linhasTabela = `
+        <tr>
+            <td>Nome do Curso</td>
+            <td>Descrição do Curso</td>
+        </tr>
+    `
+    valoresEncontrados.forEach(curso => {
+        linhasTabela += `
+            <tr>
+                <td>${curso.title}</td>
+                <td>${curso.description}</td>
+            </tr>
+        `
+    });
+
+    document.querySelector("#tabelaCurso").innerHTML = linhasTabela
+
+
+}
+
+async function buscarValoresNaAPI() {
+    const response = await fetch("https://api-dwi.herokuapp.com/api/tutorials")
+    const responseJson = await response.json()
+
+    return responseJson
+}
